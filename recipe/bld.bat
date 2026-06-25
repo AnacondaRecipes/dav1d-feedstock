a6d2ab6160ceffefaa68423da5f47399538a24ec
Eric Lundby <Eric.Lundby@gmail.com>
2026-06-25 16:19:46 -0600

    only disable asm for win-arm64
    Fixes:
    
    ERROR: Program `gas-preprocessor.pl` not found

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,14 @@
 @echo on
 
 
+set "MESON_ASM_ARGS="
+if "%target_platform%"=="win-arm64" set "MESON_ASM_ARGS=-Denable_asm=false"
+
 meson setup builddir           ^
     %MESON_ARGS%               ^
     --prefix=%LIBRARY_PREFIX%  ^
     -Denable_tests=false       ^
-    -Denable_asm=false         ^
+    %MESON_ASM_ARGS%           ^
     --buildtype=release
 if errorlevel 1 exit 1
 
